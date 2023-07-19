7953fa3ad6bc08af5d335df2c1d2afb8569c41b2
LucasT <tressoldi.lucas@gmail.com>
2023-07-19 15:53:55 -0300

    App WaterTimer done

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Timer from "./src/components/Timer";
+import { StatusBar } from "expo-status-bar";
+import * as Notify from "expo-notifications";
+import { SafeAreaView, StyleSheet } from "react-native";
+
+Notify.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldShowAlert: true,
+    shouldSetBadge: true,
+  }),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Timer />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFFAFA",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  startButton: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#0000FF",
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/Timer/index.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import * as Notify from "expo-notifications";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+export default function Timer() {
+  const time = 45 * 60;
+  const [timeLeft, setTimeLeft] = useState(time);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timerInterval;
+
+    if (isRunning) {
+      timerInterval = setInterval(() => {
+        if (timeLeft > 0) {
+          setTimeLeft((prevTime) => prevTime - 1);
+        } else {
+          clearInterval(timerInterval);
+          setIsRunning(false);
+          setTimeLeft(time);
+          sendNotification();
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(timerInterval);
+  }, [isRunning, timeLeft]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+    setTimeLeft(time);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  const sendNotification = async () => {
+    const { status } = await Notify.requestPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert("Notificações desativadas!");
+      return;
+    }
+    await Notify.scheduleNotificationAsync({
+      content: {
+        title: "WATER TIME!!",
+        body: "Time to drink some water",
+        data: [],
+      },
+      trigger: null,
+    });
+  };
+
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <Text style={{ fontSize: 24, marginBottom: 20 }}>
+        {formatTime(timeLeft)}
+      </Text>
+      <TouchableOpacity
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          backgroundColor: "#0000FF",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onPress={isRunning ? stopTimer : startTimer}
+      >
+        <MaterialCommunityIcons
+          name={isRunning ? "stop" : "water"}
+          size={50}
+          color="#FFF"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
